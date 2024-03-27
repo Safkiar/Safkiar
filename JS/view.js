ed383507ad8ff6a6b3d8a941197f8d1909a4a88b
Safkiar <m.kikowski97@gmail.com>
2024-03-27 14:57:04 +0100

    changed nav links

--- JS/view.js
+++ JS/view.js
@@ -1,22 +1,22 @@
-// SECTION FOR NAVBAR:
+// // SECTION FOR NAVBAR:
 
-document.querySelector("#Projects").addEventListener("click", function () {
-  window.location.href = "Projects.html";
-});
-document.querySelector("#Certificates").addEventListener("click", function () {
-  window.location.href = "Certificates.html";
-});
-document.querySelector("#Experience").addEventListener("click", function () {
-  window.location.href = "Experience.html";
-});
-document.querySelector("#Aboutme").addEventListener("click", function () {
-  window.location.href = "Aboutme.html";
-});
-document.querySelector("#Aboutme").addEventListener("click", function () {
-  window.location.href = "Aboutme.html";
-});
-document.querySelector("#Home").addEventListener("click", function () {
-  window.location.href = "index.html";
-});
+// document.querySelector("#Projects").addEventListener("click", function () {
+//   window.location.href = "Projects.html";
+// });
+// document.querySelector("#Certificates").addEventListener("click", function () {
+//   window.location.href = "Certificates.html";
+// });
+// document.querySelector("#Experience").addEventListener("click", function () {
+//   window.location.href = "Experience.html";
+// });
+// document.querySelector("#Aboutme").addEventListener("click", function () {
+//   window.location.href = "Aboutme.html";
+// });
+// document.querySelector("#Aboutme").addEventListener("click", function () {
+//   window.location.href = "Aboutme.html";
+// });
+// document.querySelector("#Home").addEventListener("click", function () {
+//   window.location.href = "index.html";
+// });
 
 //  THIS is LOADING SPINNER
