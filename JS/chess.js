24544999c15e3e25e143ed072e74d4359cc687f5
Safkiar <m.kikowski97@gmail.com>
2024-06-01 13:00:31 +0200

    Experience, About me pages was updated

--- JS/chess.js
+++ JS/chess.js
@@ -7,14 +7,7 @@ async function fetchUserData() {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data.chess_rapid.last.rating);
-    console.log(data.chess_rapid.record.win);
-    console.log(data.chess_rapid.record.loss);
-    console.log(data.chess_rapid.record.draw);
-    console.log(data.chess_blitz.last.rating);
-    console.log(data.chess_blitz.record.win);
-    console.log(data.chess_blitz.record.loss);
-    console.log(data.chess_blitz.record.draw);
+
     return data;
   } catch (error) {
     console.error("Could not fetch user data:", error.message);
@@ -25,19 +18,19 @@ async function fetchUserData() {
 async function updateCodingTaskElement() {
   const chessinfo = await fetchUserData();
   const winr = document.getElementsByClassName("win--r");
-  winr[0].innerText = `wins: ${chessinfo.chess_rapid.record.win.toString()}`;
+  winr[0].innerText = `wins ${chessinfo.chess_rapid.record.win.toString()}`;
   const loser = document.getElementsByClassName("lose--r");
-  loser[0].innerText = `loses: ${chessinfo.chess_rapid.record.loss.toString()}`;
+  loser[0].innerText = `loses  ${chessinfo.chess_rapid.record.loss.toString()}  `;
   const drawr = document.getElementsByClassName("draw--r");
-  drawr[0].innerText = `draws: ${chessinfo.chess_rapid.record.draw.toString()}`;
+  drawr[0].innerText = `draws ${chessinfo.chess_rapid.record.draw.toString()} `;
   const ratr = document.getElementsByClassName("elo--r");
-  ratr[0].innerText = ` ${chessinfo.chess_rapid.last.rating.toString()}`;
+  ratr[0].innerText = `${chessinfo.chess_rapid.last.rating.toString()}`;
   const winb = document.getElementsByClassName("win--b");
-  winb[0].innerText = `wins: ${chessinfo.chess_blitz.record.win.toString()}`;
+  winb[0].innerText = `wins ${chessinfo.chess_blitz.record.win.toString()}`;
   const loseb = document.getElementsByClassName("lose--b");
-  loseb[0].innerText = `loses: ${chessinfo.chess_blitz.record.loss.toString()}`;
+  loseb[0].innerText = `loses ${chessinfo.chess_blitz.record.loss.toString()}`;
   const drawb = document.getElementsByClassName("draw--b");
-  drawb[0].innerText = `draws: ${chessinfo.chess_blitz.record.draw.toString()}`;
+  drawb[0].innerText = `draws ${chessinfo.chess_blitz.record.draw.toString()}`;
   const ratb = document.getElementsByClassName("elo--b");
   ratb[0].innerText = `${chessinfo.chess_blitz.last.rating.toString()}`;
 }
